4186c6add6819dcf34bde67da575ce02562e7eee
Maanit Mehra <maanitm@gmail.com>
2016-02-08 20:56:57 +0000

    Lab 3 Files

---
+++ Lab3/dynamodata.py
@@ -0,0 +1,4 @@
+# *********************************************************************************************
+# Program to update dynamodb with latest data from mta feed. It also cleans up stale entried from db
+# Usage python dynamodata.py
+
